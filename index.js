3041cec3f64e27f9d8631e0b923527d196875ceb
Alex <observable@163.com>
2018-12-25 11:04:57 +0800

    init

---
+++ index.js
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define([], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory();
+	} else {
+		root.jsfull = factory();
+	}
+})(this, function () {
+
+	function hashCode(str) {
+		var hash = 0;
+		if (str.length == 0) return hash;
+		for (i = 0; i < str.length; i++) {
+			char = str.charCodeAt(i);
+			hash = ((hash << 5) - hash) + char;
+			hash = hash & hash;
+		}
+		return hash;
+	}
+
+	function stringify(o) {
+
+		var refs = {};
+	
+		var str = JSON.stringify(o, function (key, value) {
+	
+			if(value) {
+				if(value.constructor == RegExp || value.constructor == Function) {
+					var id = hashCode(value.constructor.prototype.toString.call(value))
+					if(!refs.hasOwnProperty(id)) {
+						refs[id] = value;
+					}
+					return "__" + id + "__";
+				}
+			}
+			return value;
+		})
+
+		console.log(refs)
+	
+		return str.replace(/"__(-?[a-z0-9]+)__"/g, function(x, hash) {
+			if(!refs.hasOwnProperty(hash)) {
+				return x;
+			}
+			var v = refs[hash];
+			if(v.constructor == RegExp) {
+				return v;
+			}
+			if(v.constructor == Function) {
+				return Function.prototype.toString.call(v);
+			}
+		});
+	}
+
+	function parse(str) {
+		return eval('(' + str + ')');
+	}
+
+	return {
+		stringify: stringify,
+		parse: parse
+	};
+});
+
